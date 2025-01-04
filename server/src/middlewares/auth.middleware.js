4e4f50cc0ae4319c8e369cc77f475ad6c3cd0dc3
WhiteSnake <whitesnake35711@gmail.com>
2025-01-04 17:44:20 +0530

    created middlewares for authentication and image upload

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,19 @@
+import { User } from "../models/user.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from 'jsonwebtoken'
+
+
+export const verifyJWT = asyncHandler( async(req,res,next)=>{
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+        if(!token) return res.status(401).json(new ApiResponse(401, {}, "Unauthorized request"))
+        const decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+        if(!user) return res.status(401).json(new ApiResponse(401, {}, "Invalid access token"))
+        req.user = user;
+        next()
+    } catch (error) {
+        return res.status(401).json(new ApiResponse(401, error.message, "Unauthorized request"))
+    }
+})
