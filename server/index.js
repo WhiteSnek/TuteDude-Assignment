0af365a3bf0da11674b3a2c1c83667bae35a0c22
WhiteSnake <whitesnake35711@gmail.com>
2025-01-04 17:44:49 +0530

    created schema for users and friend requests

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import routes from "./src/routes/index.js";
+// import routes from "./src/routes/index.js";
 
 const app = express();
 
@@ -26,7 +26,7 @@ app.use(bodyParser.raw({ limit: "10mb", type: "image/*" }));
 
 const PORT = process.env.PORT || 5000;
 connect();
-routes(app);
+// routes(app);
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "Everything fine!" });
 });
