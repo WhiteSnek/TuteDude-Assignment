15cd29c4c54460da3b7615983f3d1364187d89bc
WhiteSnake <whitesnake35711@gmail.com>
2025-01-04 14:45:04 +0530

    initialized shadcn and react-router-dom

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
+import { LandingPage } from "./pages";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const Layout: React.FC = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<App />}>
+        <Route
+          path=""
+          element={<LandingPage />}
+        />
+      </Route>
+    )
+  );
+
+  return (
+      <RouterProvider router={router} />
+  );
+};
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Layout />
+  </StrictMode>
+);
