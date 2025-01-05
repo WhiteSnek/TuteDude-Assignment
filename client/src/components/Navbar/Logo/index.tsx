5be6e7d66b84353bff5af80c91b12f19192d01c6
WhiteSnake <whitesnake35711@gmail.com>
2025-01-05 18:13:42 +0530

    named the website

--- client/src/components/Navbar/Logo/index.tsx
+++ client/src/components/Navbar/Logo/index.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Logo:React.FC = () => {
   return (
     <div className="text-2xl font-bold">
-        <Link to={"/"}>MyApp</Link>
+        <Link to={"/"}>PeerConnect</Link>
     </div>
   )
 }
